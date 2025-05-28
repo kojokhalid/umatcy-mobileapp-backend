d1ebe3662397d8aed20c06a6f0d0955e5556d3a4
kojokhalid <antohshadrack@gmail.com>
2025-05-28 12:02:58 +0000

    allowed all origin for now

--- index.ts
+++ index.ts
@@ -10,12 +10,7 @@ app.use(
   cors({
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
-    origins: [
-      "http://localhost:3000",
-      "http://localhost:5173",
-      "exp://172.20.10.3:8081",
-      "http://localhost:8081",
-    ],
+    origins: "*",
   })
 );
 
